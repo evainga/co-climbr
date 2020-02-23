5793017405e5efdaad2c72a6fca6b852721de062
evainga <ego87@web.de>
2020-02-22 19:37:37 -0600

    Initialize first setup

---
+++ src/main/java/de/coclimbr/CoClimbrApplication.java
@@ -0,0 +1,13 @@
+package de.coclimbr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoClimbrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoClimbrApplication.class, args);
+	}
+
+}
