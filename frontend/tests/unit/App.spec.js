31df916534fcf98696f4d5a2f0ba8e04e994c5ae
evainga <ego87@web.de>
2020-05-06 16:08:51 +0200

    Add first working jest test

---
+++ frontend/tests/unit/App.spec.js
@@ -0,0 +1,14 @@
+import { shallowMount } from '@vue/test-utils'
+import App from '@/App.vue'
+
+// see https://vue-test-utils.vuejs.org/guides/using-with-vue-router.html#testing-components-that-use-router-link-or-router-view
+describe('App component should', () => {
+  it('renders the correct markup without crashing', () => {
+    const wrapper = shallowMount(App, {
+      stubs: ['router-link', 'router-view']
+    })
+    expect(wrapper.html()).toContain('<div id="app">')
+    expect(wrapper.html()).toContain('<div id="nav">')
+    expect(wrapper.html()).not.toContain('<div id="nakjhkv">')
+  })
+})
