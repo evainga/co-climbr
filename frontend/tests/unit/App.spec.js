d41369f940fc121ffd7ecafea05ca15adf32b7ad
evainga <ego87@web.de>
2020-05-07 11:08:38 +0200

    activate coverage report

--- frontend/tests/unit/App.spec.js
+++ frontend/tests/unit/App.spec.js
@@ -2,7 +2,7 @@ import { shallowMount } from '@vue/test-utils'
 import App from '@/App.vue'
 
 // see https://vue-test-utils.vuejs.org/guides/using-with-vue-router.html#testing-components-that-use-router-link-or-router-view
-describe('App component should', () => {
+describe('App component', () => {
   it('renders the correct markup without crashing', () => {
     const wrapper = shallowMount(App, {
       stubs: ['router-link', 'router-view']
