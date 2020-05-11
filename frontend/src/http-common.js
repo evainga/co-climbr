0b9d9216bd9ffc1f48a0ef33df501ad0ee7540f5
evainga <ego87@web.de>
2020-05-11 11:45:04 +0200

    Delete unused axios file

--- frontend/src/http-common.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios'
-
-export const AXIOS = axios.create({
-  baseURL: 'http://localhost:8088',
-  headers: {
-    'Access-Control-Allow-Origin': 'http://localhost:8080'
-  }
-})
